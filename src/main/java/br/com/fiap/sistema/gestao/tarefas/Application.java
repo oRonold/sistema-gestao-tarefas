32e5c824f8bd93825a026c8a21d0ec8337f92093
oRonold <or040918@gmail.com>
2024-08-27 11:53:19 -0300

    Initial commit

---
+++ src/main/java/br/com/fiap/sistema/gestao/tarefas/Application.java
@@ -0,0 +1,13 @@
+package br.com.fiap.sistema.gestao.tarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
